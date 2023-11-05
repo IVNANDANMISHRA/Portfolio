9a5bba6fd3bcf9056acfc830b941cda4bc8484e4
S Nandan Mishra <115075926+IVNANDANMISHRA@users.noreply.github.com>
2023-11-06 00:45:08 +0530

    Update

---
+++ assets/js/apiCall.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contactForm = document.getElementById("contactForm");
+    const responseDiv = document.getElementById("response");
+
+    contactForm.addEventListener("submit", async function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const message = document.getElementById("message").value;
+
+        // Create an object to represent the form data
+        const formData = {
+            name: name,
+            email: email,
+            message: message,
+        };
+
+        try {
+            // Send a POST request using Axios with async/await
+            const response = await axios.post('https://port-backend-w6h2.onrender.com', formData);
+            responseDiv.innerHTML = "Message sent successfully!";
+            contactForm.reset(); // Clear the form
+        } catch (error) {
+            responseDiv.innerHTML = "Error: " + error.response.data;
+        }
+    });
+});
